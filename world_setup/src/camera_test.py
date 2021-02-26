0468590f745761dd5420cbe48f38dc371e7f4471
Viltexon <zodrinallium@gmail.com>
2021-02-26 19:06:22 +0200

    small update

--- world_setup/src/camera_test.py
+++ world_setup/src/camera_test.py
@@ -5,19 +5,20 @@ from sensor_msgs.msg import Image
 import cv2
 
 # TODO add dependencies
-from cv_bridge import CvBridge
+# TODO libgcc_s.so.1 must be installed for pthread_cancel to work
+# from cv_bridge import CvBridge
 
 
 def callback(data):
-	# TODO rviz warn No image received
-	cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-	cv2.imshow("camera1", cv_image)
+	# cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+	# cv2.imshow("camera1", cv_image)
+	rospy.loginfo("msg")
 
 
 def listener():
 	rospy.init_node('listener', anonymous=True)
 
-	bridge = CvBridge()
+	# bridge = CvBridge()
 	rospy.Subscriber("/my_robot/camera1/image_raw", Image, callback)
 	rospy.spin()
 
