7d628a86b4e3c7cd67fdadc9c7b9a23917f547af
Viltexon <zodrinallium@gmail.com>
2021-03-04 20:38:04 +0200

    attempt to avoid cv_bridge

--- world_setup/src/camera_test.py
+++ world_setup/src/camera_test.py
@@ -1,25 +1,36 @@
 #! /usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image as sensorImage
 import cv2
 
 # TODO add dependencies
 # TODO libgcc_s.so.1 must be installed for pthread_cancel to work
 # from cv_bridge import CvBridge
 
+# from PIL import Image # tmp debug
 
 def callback(data):
 	# cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+	str_msg = msg.data
+	buf = np.ndarray(shape=(1, len(str_msg)), dtype=np.uint8, buffer=msg.data)
+
+	# image = Image.fromarray(buf.astype('uint8')).convert('RGB')
+	# cv_image = np.array(image) 
+	# cv_image = cv_image[:, :, ::-1].copy()
+
+	cv_image = cv2.imdecode(buf, cv2.IMREAD_ANYCOLOR)
 	# cv2.imshow("camera1", cv_image)
+	# TODO Current thread is not the object's thread. Cannot move to target thread
 	rospy.loginfo("msg")
+	cv2.waitKey(20)
 
 
 def listener():
 	rospy.init_node('listener', anonymous=True)
 
 	# bridge = CvBridge()
-	rospy.Subscriber("/my_robot/camera1/image_raw", Image, callback)
+	rospy.Subscriber("/my_robot/camera1/image_raw", sensorImage, callback)
 	rospy.spin()
 
 
