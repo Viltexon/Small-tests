5c9b06849173a10c99953da952f49d83e7727bd3
Viltexon <zodrinallium@gmail.com>
2021-04-14 00:15:02 +0300

    Reinstall Ubuntu, install Noetic

--- world_setup/src/camera_test.py
+++ world_setup/src/camera_test.py
@@ -6,30 +6,32 @@ import cv2
 
 # TODO add dependencies
 # TODO libgcc_s.so.1 must be installed for pthread_cancel to work
-# from cv_bridge import CvBridge
+# Fixed by Ubuntu reinstall :)
+from cv_bridge import CvBridge
 
-# from PIL import Image # tmp debug
+bridge = CvBridge()
 
-def callback(data):
+def callback(msg):
 	# cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-	str_msg = msg.data
-	buf = np.ndarray(shape=(1, len(str_msg)), dtype=np.uint8, buffer=msg.data)
+	# str_msg = msg.data
+	# buf = np.ndarray(shape=(1, len(str_msg)), dtype=np.uint8, buffer=msg.data)
 
 	# image = Image.fromarray(buf.astype('uint8')).convert('RGB')
 	# cv_image = np.array(image) 
 	# cv_image = cv_image[:, :, ::-1].copy()
 
-	cv_image = cv2.imdecode(buf, cv2.IMREAD_ANYCOLOR)
+	# cv_image = cv2.imdecode(buf, cv2.IMREAD_ANYCOLOR)
 	# cv2.imshow("camera1", cv_image)
 	# TODO Current thread is not the object's thread. Cannot move to target thread
 	rospy.loginfo("msg")
-	cv2.waitKey(20)
+	cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
+	cv2.imshow("camera1", cv_image)
+	cv2.waitKey(3)
 
 
 def listener():
 	rospy.init_node('listener', anonymous=True)
 
-	# bridge = CvBridge()
 	rospy.Subscriber("/my_robot/camera1/image_raw", sensorImage, callback)
 	rospy.spin()
 
